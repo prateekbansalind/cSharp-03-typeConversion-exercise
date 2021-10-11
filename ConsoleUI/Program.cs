c9001bdcca9f10b0f53965107ff4df131fe21ced
pbansal16 <prateekbansalind@gmail.com>
2021-10-11 21:04:58 +1300

    Initial Commit : Program to get user input if he/she allow into casino

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Progrma
+    {
+        static void Main(string[] args)
+        {
+            // program to demonstrate concept of type conversion
+
+            //console program to get user age and decide if the user permissible to enter into the casino
+
+            Console.WriteLine("Welcome to the Casino");
+            Console.WriteLine("");
+
+            Console.Write("Enter your age (in integer): ");
+
+            string ageText = Console.ReadLine();
+
+            bool isValidAge = int.TryParse(ageText, out int age);
+
+            if (age == 18 && age >= 18)
+            {
+                Console.WriteLine("Welcome, You are allowed to enter into the casino.");
+            }
+            else
+            {
+                Console.WriteLine("You are not 18 and considered as underage. However, you are not allowed inside the casino.");
+                
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
